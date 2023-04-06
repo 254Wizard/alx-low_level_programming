4eab0d569a8dfe27740777148f7ceebe8add7b39
254Wizard <iandennis254@gmail.com>
2023-04-06 07:16:54 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,12 @@
  */
 void print_binary(unsigned long int j)
 {
-	unsigned long int mask = 1;
-
-	mask = mask << (sizeof(unsigned long int) * 8 - 1);
-
-	while (mask != 0)
+	if (j >> 0)
 	{
-		if ((j & mask) != 0)
-			_putchar('1');
-		else
-			_putchar('0');
-
-	mask = mask >> 1;
+		if (j >> 1)
+			print_binary(j >> 1);
+		_putchar((j & 1) + '0');
 	}
+	else
+		_putchar('0');
 }
